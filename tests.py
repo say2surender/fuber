ce0574b9217ea2ddccad66cef43fb9b63561d5bd
say2surender <say2surender@gmail.com>
2017-09-26 01:23:04 +0530

    unit tests for other independant functions

--- tests.py
+++ tests.py
@@ -1,8 +1,22 @@
 import pytest
 import utils
+import service
 
+# unit tests for utils-----------//
 def test_h_loc():
     assert utils.h_loc({"lat": 2, "long": 4}) == 6
 
 def test_eu_distance():
     assert utils.eu_distance({"lat": 2, "long": 4}, {"lat": 2, "long": 6}) == 2
+
+
+# unit tests for service-----------//
+def test_ping():
+    assert service.ping() == "ping success"
+
+def test_calculate_available_taxis():
+    assert type(service.calculate_available_taxis()) == type([])
+
+def test_attrs_calculate_available_taxis():
+    assert not (hasattr(service.calculate_available_taxis()[0], "taxi_id") and hasattr(service.calculate_available_taxis()[0], "category") and hasattr(service.calculate_available_taxis()[0], "curr_loc")
+)
