35ba64e5bc9199d3508da82ca3292c6c49443a7f
say2surender <say2surender@gmail.com>
2017-09-26 00:44:42 +0530

    exception handling done

--- app.py
+++ app.py
@@ -2,30 +2,72 @@ from flask import Flask, jsonify, request
 import service
 APP = Flask(__name__)
 
+service.calculate_available_taxis()
 
 @APP.route('/')
 def ping():
+    """ A health check for app
+
+    """
     return service.ping()
-
-@APP.route('/initiate/')
-def initiate():
-    return jsonify(service.calculate_available_taxis())
-
-@APP.route('/taxis/all')
-def print_taxis():
-    return service.print_db("taxis")
 
 @APP.route('/user/taxi/request/', methods=['POST'])
 def request_taxi():
+    """ Returns a available taxi, if it is within 50km range
+    ---
+        parameters:
+          - in: body
+            name: body
+            description: No Parameters required
+        responses:
+            success:
+                description: Response sent
+                schema:
+                    type: "dict"
+            error:
+                description: Error sent
+                schema:
+                    type: "str"
+                value:
+                    "No taxis around you"
+    """
+    error = None
+    code = 200
     if hasattr(request, 'json') and request.json is not None:
-        ride = service.request_taxi(request.json["loc"], request.json["type"])
-    return jsonify(ride)
+        ride, error = service.request_taxi(request.json["loc"], request.json["type"])
+        if error is not None:
+            code = 404
+            ride = error
+    return jsonify(ride), code
 
 @APP.route('/user/taxi/end/', methods=['POST'])
 def end_ride():
+    """ Ends a ride, returns fare and distance_covered, and other ride details
+    ---
+        parameters:
+          - in: body
+            name: body
+            description: No Parameters required
+        responses:
+            success:
+                description: Response sent
+                schema:
+                    type: "dict"
+            error:
+                description: Error sent
+                schema:
+                    type: "str"
+                value:
+                    "No ride to end"
+    """
+    error = None
+    code = 200
     if hasattr(request, 'json') and request.json is not None:
-        ride = service.end_ride(request.json)
-    return jsonify(ride)
+        ride, error = service.end_ride(request.json)
+        if error is not None:
+            ride = error
+            code = 400
+    return jsonify(ride), code
 
 if __name__ == "__main__":
     APP.run(
--- service.py
+++ service.py
@@ -8,9 +8,15 @@ from dateutil import parser
 with open('data.json') as data_file:
     db = json.load(data_file)
 
+# Global Variables, which can be kept in memory
 available_taxis = []
 live_rides = []
 
+# health check service
+def ping():
+    return "ping success"
+
+# One time initiation function to load the available_taxis to in memory db
 def calculate_available_taxis():
     for taxi in db["taxis"]:
         if taxi["is_available"] == True:
@@ -21,70 +27,76 @@ def calculate_available_taxis():
                                 })
     return available_taxis
 
-def request_taxi(loc, type):
-    global live_rides
-    global available_taxis
 
+def request_taxi(loc, type):
+
+    # setting default responses
+    ride = error = None
     nearest_taxi = None
+
+    # minimum radius for searching a taxi is 50km
     nearest_distance = 50
-    print(loc, type)
-    for taxi in available_taxis:
-        if taxi["category"] == type:
-            this_taxi_distance = utils.eu_distance(loc, taxi["curr_loc"])
-            if this_taxi_distance < nearest_distance:
-                nearest_distance = this_taxi_distance
-                nearest_taxi = taxi
-    if nearest_taxi is not None:
-        available_taxis.remove(nearest_taxi)
-        ride = {
-        "id": 1,
-        "taxi": nearest_taxi,
-        "start_time": datetime.datetime.utcnow(),
-        "end_time": 0,
-        "start_loc": loc,
-        "end_loc": {
-                    "lat": 0,
-                    "long": 0
-                    }
-        }
-        live_rides.append(ride)
-        return ride
-    else:
-        return "No rides near you, Sorry."
+
+    try:
+        # getting nearest taxi
+        for taxi in available_taxis:
+            if taxi["category"] == type:
+                this_taxi_distance = utils.eu_distance(loc, taxi["curr_loc"])
+                if this_taxi_distance < nearest_distance:
+                    nearest_distance = this_taxi_distance
+                    nearest_taxi = taxi
+
+        # initiate a live_ride and add to live_rides
+        if nearest_taxi is not None:
+            available_taxis.remove(nearest_taxi)
+            ride = {
+            "id": 1,
+            "taxi": nearest_taxi,
+            "start_time": datetime.datetime.utcnow(),
+            "end_time": 0,
+            "start_loc": loc,
+            "end_loc": {
+                        "lat": 0,
+                        "long": 0
+                        }
+            }
+            live_rides.append(ride)
+        else:
+            raise LookupError("Sorry, Taxi Not found within 50km range")
+    except LookupError as err:
+        error = err.args
+    return ride, error
 
 def end_ride(ride):
-    live_ride_found = False
-    global live_rides
     global available_taxis
+    # setting default responses
+    error = None
+    live_ride_found = False
+    # de-allocating a taxi, making it available for further rides
     available_taxis.append(
                         {"taxi_id": ride["taxi"]["taxi_id"],
                         "category": ride["taxi"]["category"],
                         "curr_loc": ride["end_loc"]
                         })
-    live_rides = [x for x in live_rides if x['id'] not in [ride]]
-    for i in range(len(live_rides)):
-        if live_rides[i]['id'] == ride["id"]:
-            del live_rides[i]
-            live_ride_found = True
-            break
-    print(live_rides)
-    if live_ride_found:
-        ride["time_elapsed"] = (parser.parse(ride["start_time"]) - parser.parse(ride["start_time"])).total_seconds()
-        ride["distance_covered"] = utils.eu_distance(ride["start_loc"], ride["end_loc"])
-        ride["fare"] = ride["time_elapsed"] + ride["distance_covered"] * 2
-        with open("rides.log", "a") as log:
-            log.write(str(ride["id"])+" - "+str(ride["fare"])+" dodgecoins")
-        return ride
-    else:
-        return "Not a valid ride"
 
-def ping():
-    return "ping success"
+    try:
+        # remove ride from live_rides
+        for i in range(len(live_rides)):
+            if live_rides[i]['id'] == ride["id"]:
+                del live_rides[i]
+                live_ride_found = True
+                break
 
-def print_db(key):
-    pprint (db[key])
-    return jsonify(db[key])
-
-def add_taxi(taxi):
-    db["taxis"].append(taxi)
-    return success
+        # generate meta of ride, for fare calculation and write fare in log
+        if live_ride_found:
+            ride["time_elapsed"] = (parser.parse(ride["start_time"]) - parser.parse(ride["start_time"])).total_seconds()
+            ride["distance_covered"] = utils.eu_distance(ride["start_loc"], ride["end_loc"])
+            ride["fare"] = ride["time_elapsed"] + ride["distance_covered"] * 2
+            with open("rides.log", "a") as log:
+                log.write(str(ride["id"])+" - "+str(ride["fare"])+" dodgecoins")
+                log.write("\n")
+        else:
+            raise LookupError("Sorry, No such ride found")
+    except LookupError as err:
+        error = err.args
+    return ride, error
