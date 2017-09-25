b7862e7a9f0ef3c6f8cd90b43961a46b0f89c779
say2surender <say2surender@gmail.com>
2017-09-25 08:08:18 +0530

    taxi-request done

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ def initiate():
 def print_taxis():
     return service.print_db("taxis")
 
-@APP.route('/admin/taxi/', methods=['POST'])
-def add_taxi():
+@APP.route('/user/taxi/request', methods=['POST'])
+def request_taxi():
+    if hasattr(request, 'json') and request.json is not None:
+        nearest_taxi, error = service.get_taxi(request.json["loc"], request.json["type"])
+        if nearest_taxi is not None:
+            return "Taxi hailed succesfully " + nearest_taxi["id"]
+
     return service.print_db("taxis")
 
 
